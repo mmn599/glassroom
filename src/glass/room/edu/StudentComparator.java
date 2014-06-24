0f1f53d9c46e3aa347ba46bd23339329edfa0333
Matthew Normyle <mnormyle@salesforce.com>
2014-06-24 01:05:42 -0700

    Changed gitignore and huge updates to the flow in suggeststudentactivity

--- src/glass/room/edu/StudentComparator.java
+++ src/glass/room/edu/StudentComparator.java
@@ -5,22 +5,15 @@ import java.util.Comparator;
 public class StudentComparator implements Comparator<Student> {
 
 	String subject;
-	
+
 	public StudentComparator(String subject) {
 		this.subject = subject;
 	}
-	
+
 	@Override
 	public int compare(Student lhs, Student rhs) {
-		if(subject.equals("Mathematics")) {
-			//logic
-		}
-		else if(subject.equals("Literature")) {
-			//logic
-		}
-		else if(subject.equals("Biology")) {
-			//logic
-		}
+		return Integer.compare(lhs.getPerformance(subject).getCorrect(), rhs
+				.getPerformance(subject).getCorrect());
 	}
 
 }
